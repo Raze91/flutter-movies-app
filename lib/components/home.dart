abd06f41417f474e566c5b561bfdd0ace40ea7e5
Drallireeh <kenny.herillard@hotmail.fr>
2022-04-12 11:19:52 +0200

    Move tmdb api inside home component

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/cupertino.dart';
+import 'package:tmdb_api/tmdb_api.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [Text('Home')],
-        )
-      ],
+    final tmdbWithLogs = TMDB(
+      ApiKeys('0afa2097e0c2ef64f9ce5f27c0e16c99', 'apiReadAccessTokenv4'),
+      logConfig: const ConfigLogger.showAll(),
     );
+
+    return FutureBuilder(
+        future: tmdbWithLogs.v3.movies.getPopular(),
+        builder: (context, AsyncSnapshot<Map<dynamic, dynamic>> snapshot) {
+          if (snapshot.hasData) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [Text('Home')],
+                )
+              ],
+            );
+          } else {
+            return Row();
+          }
+        });
   }
 }
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,31 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:tmdb_api/tmdb_api.dart';
 import 'package:flutter_movies_app/components/home.dart';
 import 'package:flutter_movies_app/components/profile.dart';
 import 'package:flutter_movies_app/components/search.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
-
-// @override
-//   Widget build(BuildContext context) {
-//     final tmdbWithLogs = TMDB(
-//       ApiKeys('0afa2097e0c2ef64f9ce5f27c0e16c99', 'apiReadAccessTokenv4'),
-//       logConfig: const ConfigLogger.showAll(),
-//     );
-
-//     return FutureBuilder(
-//         future: tmdbWithLogs.v3.movies.getPopular(),
-//         builder: (context, AsyncSnapshot<Map<dynamic, dynamic>> snapshot) {
-//           if (snapshot.hasData) {
-//             print(snapshot.data);
-//             return Scaffold(
-//               appBar: AppBar(title: const Text("Flutter Movies App")),
-//             );
-//           } else {
-//             return const CircularProgressIndicator();
-//           }
-//         });
 
   @override
   State<StatefulWidget> createState() => _HomePageState();
