2975cfb14b5f98a6ab57ab6525ce6a9732a1f5a4
Drallireeh <kenny.herillard@hotmail.fr>
2022-04-12 11:08:20 +0200

    Add tmdb api and get movies from it

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:tmdb_api/tmdb_api.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Flutter Movies App")
-      ),
+    final tmdbWithLogs = TMDB(
+      ApiKeys('0afa2097e0c2ef64f9ce5f27c0e16c99', 'apiReadAccessTokenv4'),
+      logConfig: const ConfigLogger.showAll(),
     );
+
+    return FutureBuilder(
+        future: tmdbWithLogs.v3.movies.getPopular(),
+        builder: (context, AsyncSnapshot<Map<dynamic, dynamic>> snapshot) {
+          if (snapshot.hasData) {
+            print(snapshot.data);
+            return Scaffold(
+              appBar: AppBar(title: const Text("Flutter Movies App")),
+            );
+          } else {
+            return const CircularProgressIndicator();
+          }
+        });
   }
 }
