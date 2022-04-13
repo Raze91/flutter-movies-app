7a7fd47a9cd65843b8debcd2c8ba679051c392e7
Drallireeh <kenny.herillard@hotmail.fr>
2022-04-13 14:33:19 +0200

    Format date inside details page

--- lib/screens/movie_details.dart
+++ lib/screens/movie_details.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import 'package:intl/intl.dart';
 import '../components/darktheme/dark_theme_provider.dart';
 
 class MovieDetails extends StatelessWidget {
@@ -13,6 +13,12 @@ class MovieDetails extends StatelessWidget {
     final genresString = args['genresString'];
 
     final themeChange = Provider.of<DarkThemeProvider>(context);
+    DateTime date;
+    if (movie["release_date"] != null ) {
+      date = DateTime.parse(movie["release_date"]);
+    } else {
+      date = DateTime.parse(movie['first_air_date']);
+    }
 
     return Scaffold(
       appBar: AppBar(title: const Text('Movie Details'), actions: [
@@ -48,7 +54,7 @@ class MovieDetails extends StatelessWidget {
               child: Text(genresString)),
           Padding(
               padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Text(movie['release_date'] ?? movie['first_air_date'])),
+              child: Text(DateFormat.yMMMMd().format(date))),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
             child: Text(movie['overview']),
