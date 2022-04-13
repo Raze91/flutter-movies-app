0d560cefd92db5431dc3e2862f1e54844af49253
Raze91 <hattab_sami@yahoo.fr>
2022-04-13 13:44:14 +0200

    improve overall styles for the movies components

--- lib/components/movies/movie_card.dart
+++ lib/components/movies/movie_card.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class MovieCard extends StatelessWidget {
   final movie;
   final String genresString;
 
-  const MovieCard({Key? key, required this.movie, required this.genresString})
+  const MovieCard(
+      {Key? key, required this.movie, required this.genresString})
       : super(key: key);
 
   @override
@@ -13,19 +15,32 @@ class MovieCard extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Column(
         children: [
+          const SizedBox(height: 10),
           Container(
             height: 200,
             decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
                 image: DecorationImage(
+                    fit: BoxFit.cover,
                     image: NetworkImage('https://image.tmdb.org/t/p/w500' +
-                        movie['poster_path']))),
+                        movie['backdrop_path']))),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Text(
+              movie['title'] ?? movie['name'],
+              style: const TextStyle(fontSize: 12, overflow: TextOverflow.ellipsis, fontWeight: FontWeight.w600, letterSpacing: 1),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 7),
+            child: Text(
+              genresString,
+              style: const TextStyle(fontSize: 12),
+            ),
           ),
           Text(
-            movie['title'] ?? movie['name'],
-            style: const TextStyle(fontSize: 12),
-          ),
-          Text(
-            genresString,
+            movie['release_date'] ?? movie['first_air_date'],
             style: const TextStyle(fontSize: 12),
           )
         ],
