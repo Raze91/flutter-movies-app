e22acac87bf6698d77a9ed83485159f1fd72926a
Raze91 <hattab_sami@yahoo.fr>
2022-04-13 14:32:11 +0200

    add noImage assets in case the movie has no backdrop image in the search results

--- lib/components/movies/movie_card.dart
+++ lib/components/movies/movie_card.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -5,8 +7,7 @@ class MovieCard extends StatelessWidget {
   final movie;
   final String genresString;
 
-  const MovieCard(
-      {Key? key, required this.movie, required this.genresString})
+  const MovieCard({Key? key, required this.movie, required this.genresString})
       : super(key: key);
 
   @override
@@ -16,20 +17,27 @@ class MovieCard extends StatelessWidget {
       child: Column(
         children: [
           const SizedBox(height: 10),
-          Container(
-            height: 200,
-            decoration: BoxDecoration(
+          SizedBox(
+              height: 200,
+              child: ClipRRect(
                 borderRadius: BorderRadius.circular(20),
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage('https://image.tmdb.org/t/p/w500' +
-                        movie['backdrop_path']))),
-          ),
+                child: movie['backdrop_path'] == null
+                    ? Image.asset('noImage.png', fit: BoxFit.cover)
+                    : Image.network(
+                        'https://image.tmdb.org/t/p/w500' +
+                            movie['backdrop_path'],
+                        fit: BoxFit.cover,
+                      ),
+              )),
           Padding(
             padding: const EdgeInsets.only(top: 10),
             child: Text(
               movie['title'] ?? movie['name'],
-              style: const TextStyle(fontSize: 12, overflow: TextOverflow.ellipsis, fontWeight: FontWeight.w600, letterSpacing: 1),
+              style: const TextStyle(
+                  fontSize: 12,
+                  overflow: TextOverflow.ellipsis,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1),
             ),
           ),
           Padding(
