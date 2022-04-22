012ce4c9745ec1fe11e2381540d5c498b7165c30
Raze91 <hattab_sami@yahoo.fr>
2022-04-22 11:41:44 +0200

    style improvements for the details page

--- lib/screens/movie_details.dart
+++ lib/screens/movie_details.dart
@@ -32,70 +32,82 @@ class MovieDetails extends StatelessWidget {
               themeChange.darkTheme = value;
             })
       ]),
-      body: Column(
-        children: [
-          SizedBox(height: 10),
-          Container(
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: movie['backdrop_path'] == null
-                    ? Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child:
-                                  Image.asset('noImage.png', fit: BoxFit.cover),
-                            )
-                          ])
-                    : FadeInImage(
-                        height: 450,
-                        width: 300,
-                        image: NetworkImage(
-                          'https://image.tmdb.org/t/p/w500/' +
-                              movie['poster_path'],
-                        ),
-                        fit: BoxFit.cover,
-                        placeholder: const AssetImage('loading.gif'),
-                      )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
-            child: Text(movie['title'] ?? movie['name'],
-                style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                    letterSpacing: 1.2)),
-          ),
-          Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Text(genresString != '' ? genresString : 'No genres')),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(movie['vote_average'] != null
-                    ? 'Score : ' + movie['vote_average'].toString() + ' /10'
-                    : 'No score found'),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 4),
-                  child: Icon(Icons.star),
-                )
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            Container(
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: movie['backdrop_path'] == null
+                      ? Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Image.asset('noImage.png',
+                                    fit: BoxFit.cover),
+                              )
+                            ])
+                      : FadeInImage(
+                          height: 450,
+                          width: 300,
+                          image: NetworkImage(
+                            'https://image.tmdb.org/t/p/w500/' +
+                                movie['poster_path'],
+                          ),
+                          fit: BoxFit.cover,
+                          placeholder: const AssetImage('loading.gif'),
+                        )),
             ),
-          ),
-          Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Text(date != null
-                  ? 'Release date : ' + DateFormat.yMMMMd().format(date)
-                  : "Release date : No release date")),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text(movie['overview'] ?? "No description available",
-                style: TextStyle(letterSpacing: 1.2, wordSpacing: 1)),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
+              child: Text(movie['title'] ?? movie['name'],
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                      letterSpacing: 1.2)),
+            ),
+            Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(genresString != '' ? genresString : 'No genres',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        letterSpacing: 1.2))),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    movie['vote_average'] != null
+                        ? 'Score : ' + movie['vote_average'].toString() + ' /10'
+                        : 'No score found',
+                    style: const TextStyle(fontSize: 18, letterSpacing: 1.2),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 4),
+                    child: Icon(Icons.star),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                    date != null
+                        ? 'Release date : ' + DateFormat.yMMMMd().format(date)
+                        : "Release date : No release date",
+                    style: const TextStyle(fontSize: 17, letterSpacing: 1.2))),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: Text(movie['overview'] ?? "No description available",
+                  style: TextStyle(
+                      letterSpacing: 1.2, wordSpacing: 1, fontSize: 18)),
+            ),
+          ],
+        ),
       ),
     );
   }
